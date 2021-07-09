374eb3d2a505722ca1dacbd28ea0e376893830c6
Madhumita G <madhumitagiribabu25@gmail.com>
2021-07-09 21:56:41 +0530

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,127 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
+
+app = dash.Dash()
+server = app.server
+
+scaler=MinMaxScaler(feature_range=(0,1))
+
+
+
+df_nse = pd.read_csv("NSE-Tata2.csv")
+
+df_nse["Date"]=pd.to_datetime(df_nse.Date,format="%Y-%m-%d")
+df_nse.index=df_nse['Date']
+
+
+data=df_nse.sort_index(ascending=True,axis=0)
+new_data=pd.DataFrame(index=range(0,len(df_nse)),columns=['Date','Close'])
+
+for i in range(0,len(data)):
+    new_data["Date"][i]=data['Date'][i]
+    new_data["Close"][i]=data["Close"][i]
+
+new_data.index=new_data.Date
+new_data.drop("Date",axis=1,inplace=True)
+
+dataset=new_data.values
+
+train=dataset[0:987,:]
+valid=dataset[987:,:]
+
+scaler=MinMaxScaler(feature_range=(0,1))
+scaled_data=scaler.fit_transform(dataset)
+
+x_train,y_train=[],[]
+
+for i in range(60,len(train)):
+    x_train.append(scaled_data[i-60:i,0])
+    y_train.append(scaled_data[i,0])
+    
+x_train,y_train=np.array(x_train),np.array(y_train)
+
+x_train=np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
+lstm_model=load_model("saved_lstm_model.h5")
+
+inputs=new_data[len(new_data)-len(valid)-60:].values
+inputs=inputs.reshape(-1,1)
+inputs=scaler.transform(inputs)
+
+X_test=[]
+for i in range(60,inputs.shape[0]):
+    X_test.append(inputs[i-60:i,0])
+X_test=np.array(X_test)
+
+X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
+closing_price=lstm_model.predict(X_test)
+closing_price=scaler.inverse_transform(closing_price)
+
+train=new_data[:987]
+valid=new_data[987:]
+valid['Predictions']=closing_price
+
+
+app.layout = html.Div([
+   
+    html.H1("Stock Price Analysis Dashboard", style={"textAlign": "center"}),
+   
+    dcc.Tabs(id="tabs", children=[
+       
+        dcc.Tab(label='ASIANPAINT- Stock Data',children=[
+			html.Div([
+				html.H2("Actual closing price",style={"textAlign": "center"}),
+				dcc.Graph(
+					id="Actual Data",
+					figure={
+						"data":[
+							go.Scatter(
+								x=train.index,
+								y=valid["Close"],
+								mode='markers'
+							)
+
+						],
+						"layout":go.Layout(
+							title='scatter plot',
+							xaxis={'title':'Date'},
+							yaxis={'title':'Closing Rate'}
+						)
+					}
+
+				),
+				html.H2("LSTM Predicted closing price",style={"textAlign": "center"}),
+				dcc.Graph(
+					id="Predicted Data",
+					figure={
+						"data":[
+							go.Scatter(
+								x=valid.index,
+								y=valid["Predictions"],
+								mode='markers'
+							)
+
+						],
+						"layout":go.Layout(
+							title='scatter plot',
+							xaxis={'title':'Date'},
+							yaxis={'title':'Closing Rate'}
+						)
+					}
+
+				)				
+			])        		
+
+        ])
+    ])
+])
+        
+if __name__=='__main__':
+	app.run_server(debug=False)            
